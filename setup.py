47ca5243c58fccdc86ac7112c5ddf512f76b2119
Blazhe Manev <blaze_manev@yahoo.com>
2025-01-19 19:08:12 +0100

    fix the type

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="influential-analysis",
-    version="0.1",
-    packages=find_packages(),
+    name="influential_analysis",
+    version="0.1.1",
+    packages=find_packages(),  # ✅ Ensure this detects influential_analysis
     install_requires=[
         "numpy",
         "pandas",
@@ -12,14 +12,13 @@ setup(
         "scikit-learn",
         "fairlearn"
     ],
-    author="Blazhe manev",
+    author="Blazhe Manev",
     description="A library for finding influential instances in ML models",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/BlazheManev/bm-influential-instance-analyzer",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: None",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
